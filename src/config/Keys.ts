41927905a6f053a3c576ac469c0c3b64db7691d0
yeraldinarboleda <yeraldin.1701811368@ucaldas.edu.co>
2021-04-16 10:57:02 -0500

    identificacion de usuario

--- src/config/Keys.ts
+++ src/config/Keys.ts
@@ -1,4 +1,6 @@
 export namespace Keys {
   export const origenCorreoElecronico = 'geidy.1701810264@ucaldas.edu.co'
   export const asuntoNuevoUsuario = '[Nuevo Usuario Constructora] Mensaje de Bienvenida';
+  export const tiempoVencimientoJWT = Math.floor(Date.now() / 1000) + (60 * 60 * 8);
+  export const claveSecretaJWT = ' jwt@prog3*';
 }
--- src/services/sesion.service.ts
+++ src/services/sesion.service.ts
@@ -1,10 +1,23 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
-
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {Keys as llaves} from '../config/Keys';
+import {Usuario} from '../models';
+const jwt = require('jsonwebtoken');
 @injectable({scope: BindingScope.TRANSIENT})
 export class SesionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */) { }
 
   /*
-   * Add service methods here
+   * Funcion que genera un token JWT
    */
+
+  GenerarToken(usuario: Usuario): string {
+    let tk = jwt.sign({
+      exp: llaves.tiempoVencimientoJWT,
+      data: {
+        username: usuario.nombres,
+        role: usuario.rol
+      }
+    }, llaves.claveSecretaJWT);
+    return tk;
+  }
 }
